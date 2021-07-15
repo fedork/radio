b7eb8f8fa19e45f456c1f04cd32240d7190cd709
fedor.karpelevitch <fedor.karpelevitch@workday.com>
2021-07-15 08:32:19 -0700

    update pareto

--- radioSbPareto.c
+++ radioSbPareto.c
@@ -7,19 +7,19 @@ int main(int argc, char **argv){
     int sb[1];
     int k, n1, n2;
     k=8;
-        n1=min(1+(1<<k), MAX_N-1);
-        n2=10;
-        while (n1>=n2) {
-            sb[0]=getSbb(n1,n2);
-            if (canSolveB(sb,1,k,NO_DEADLINE)) {
-                printf("result   can solve ");
-                n2++;
-            } else {
-                printf("result can't solve ");
-                n1--;
-            }
-            printSb(sb, 1);
-            printf(" in %d\n", k);
+    n1=114;
+    n2=24;
+    while (n2>9) {
+        sb[0]=getSbb(n1,n2);
+        if (canSolveB(sb,1,k,NO_DEADLINE)) {
+            printf("result   can solve ");
+            n1++;
+        } else {
+            printf("result can't solve ");
+            n2--;
         }
+        printSb(sb, 1);
+        printf(" in %d\n", k);
+    }
     return 0;
 }
