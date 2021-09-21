55b52a48c5c3643f837f18a5a996825f9a7262cb
fedor.karpelevitch <fedor.karpelevitch@workday.com>
2021-09-21 09:48:48 -0700

    no deadline for subplots

--- radiobase.c
+++ radiobase.c
@@ -50,6 +50,7 @@
 #define CACHE_ONLY 1
 #define NO_DEADLINE 2
 #define FAST_ONLY 3
+#define SUBSPLIT_DEADLINE NO_DEADLINE
 
 typedef struct {
     int size;
@@ -627,9 +628,9 @@ int canSolveB(int *sb, int size, int k, clock_t parent_deadline){
                     debug_printf(" i=%d p0=%d p1=%d p2=%d\n", i, p0,p1,p2);
 #endif
                     if ((p0 <= max_pairs_1) && (p1 <= max_pairs_1) && (p2 <= max_pairs_1)
-                        && (cs0 = canSolveB(sb0, i+1, k_1, CACHE_ONLY))
-                        && (cs2 = canSolveB(sb2, i+1, k_1, CACHE_ONLY))
-                        && (cs1 = canSolveB(sb1, (i+1) * 2, k_1, CACHE_ONLY))
+                        && (cs0 = canSolveB(sb0, i+1, k_1, SUBSPLIT_DEADLINE)
+                        && (cs2 = canSolveB(sb2, i+1, k_1, SUBSPLIT_DEADLINE))
+                        && (cs1 = canSolveB(sb1, (i+1) * 2, k_1, SUBSPLIT_DEADLINE))
                         )
                     {
                         debug_printf("can solve\n");
