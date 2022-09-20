c1966f60fbe9100437293f9d996e1f82c7953af4
fedor.karpelevitch <fedor.karpelevitch@workday.com>
2022-09-19 20:00:23 -0700

    add file parsing to radio_full

--- radio_full.c
+++ radio_full.c
@@ -9,16 +9,22 @@ int main(int argc, char **argv){
         printf("insufficient args\n");
         exit(1);
     }
+    
+    int offset = 0;
+    if (argc % 2 == 1) {
+        offset = 1;
+    }
 
-    int k = atoi(argv[1]);
-    int size = (argc - 2)/2;
+    int k = atoi(argv[offset+1]);
+    int size = (argc - offset - 2)/2;
     int sb[size];
     int i;
     for (i=0; i<size; i++) {
-        sb[i] = getSbb(atoi(argv[2+i*2]), atoi(argv[3+i*2]));
+        sb[i] = getSbb(atoi(argv[offset + 2 + i*2]), atoi(argv[offset+ 3 + i*2]));
     }
     printf("k=%d ", k);
     printSb(sb, size);
     printf("\n");
+    if (offset>0) parse_file(argv[1]);
     all_solutions(sb, size, k);
 }
